7c2ab043d47f69515cce9eeababa283efb7bb725
PelFritz <fpeleke@icloud.com>
2024-10-03 08:36:52 +0200

    updated

--- arabidopsis/visualize_results.py
+++ arabidopsis/visualize_results.py
@@ -3,12 +3,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 pd.options.display.width = 0
+sns.set_context(context="paper", rc={"font.size":40,"axes.titlesize":14,"axes.labelsize":14})
+
 
 palette = ['#FC4100', '#FFC55A', '#00215E']
 def boxplot_performance():
     data = []
+    fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(9, 5))
     for log_file in os.listdir('results'):
-        if log_file.endswith('.log'):
+        if log_file.endswith('.log') and log_file.startswith('csv'):
             log_df = pd.read_csv(f'results/{log_file}')
             log_df.sort_values(by='val_loss', ascending=True, inplace=True)
             log_df['case'] = [log_file.split('_')[-1].split('.')[0]]*log_df.shape[0]
@@ -35,22 +38,23 @@ def boxplot_performance():
 
     hue_order = sorted(data['model type'].unique())
     order = sorted(data['region'].unique())
-    for metric in ['auPR', 'auROC', 'weighted auPR']:
+    for idx, metric in enumerate(['auPR', 'weighted auPR',  'auROC']):
         data_met = data[data['metric'] == metric]
-        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(4, 8))
-        g = sns.barplot(data=data_met, y='score', x='region', hue='model type', ax=ax, capsize=0.3,
+        print(data.head())
+        data_met.columns = [metric, 'model type', 'metric', 'region']
+        g = sns.barplot(data=data_met, y=metric, x='region', hue='model type', ax=ax[idx], capsize=0.3,
                         palette=palette, hue_order=hue_order,
                         edgecolor='white', order=order)
-        sns.stripplot(y='score', x='region', hue='model type', data=data_met, dodge=True, ax=g, edgecolor='k',
+        sns.stripplot(y=metric, x='region', hue='model type', data=data_met, dodge=True, ax=g, edgecolor='k',
                       linewidth=0.6, palette=palette, alpha=0.5,
                       hue_order=hue_order)
-        ax.yaxis.grid(True, alpha=0.3)
-        ax.xaxis.grid(True, alpha=0.3)
-        ax.set_axisbelow(True)
-        ax.set_ylim(0.0, 1.0)
-        plt.savefig(f"results/Figures/performance_multilabel_{metric}.svg", bbox_inches='tight',
-                    dpi=300, format='svg')
-        plt.show()
-
+        ax[idx].yaxis.grid(True, alpha=0.3)
+        ax[idx].xaxis.grid(True, alpha=0.3)
+        ax[idx].set_axisbelow(True)
+        ax[idx].set_ylim(0.0, 1.0)
+    fig.tight_layout()
+    plt.savefig(f"results/Figures/performance_multilabel_classification.svg", bbox_inches='tight',
+                dpi=300, format='svg')
+    plt.show()
 
 boxplot_performance()
